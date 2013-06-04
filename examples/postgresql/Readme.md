d892c8f2ef9f04853319aeae064a21274b529998
Thom Seddon <thom@nightworld.com>
2013-06-04 11:27:02 +0100

    Add PostgreSQL model example
    Closes #1

---
+++ examples/postgresql/Readme.md
@@ -0,0 +1,25 @@
+# PostgreSQL Example
+
+See schema.sql for the tables referred to in this example
+
+The object exposed in model.js could be directly passed into the model paramater of the config
+object when initiating.
+
+For example:
+
+```js
+...
+
+app.configure(function() {
+	var oauth = oauthserver({
+		model: require('./model'),
+		grants: ['password'],
+		debug: true
+	});
+	app.use(express.bodyParser());
+	app.use(oauth.handler());
+	app.use(oauth.errorHandler());
+});
+
+...
+```
---
+++ examples/postgresql/model.js
@@ -0,0 +1,83 @@
+var pg = require('pg'),
+	model = module.exports,
+	connString = process.env.DATABASE_URL;
+
+/*
+ * Required
+ */
+
+model.getAccessToken = function (bearerToken, callback) {
+	pg.connect(connString, function (err, client, done) {
+		if (err) return callback(err);
+		client.query('SELECT access_token, client_id, expires, user_id FROM oauth_access_tokens ' +
+				'WHERE access_token = $1', [bearerToken], function (err, result) {
+			// This object will be exposed in req.oauth.token
+			// The user_id field will be exposed in req.user (req.user = { id: "..." }) however if an
+			// explicit user object is included (token.user) it will be exposed in req.user instead
+			callback(err, result.rowCount ? result.rows[0] : false);
+			done();
+		});
+	});
+};
+
+model.getClient = function (clientId, clientSecret, callback) {
+	pg.connect(connString, function (err, client, done) {
+		if (err) return callback(err);
+		client.query('SELECT client_id, client_secret, redirect_uri FROM oauth_clients WHERE ' +
+				'client_id = $1 AND client_secret = $2', [clientId, clientSecret],
+				function (err, result) {
+			// This object will be exposed in req.oauth.client
+			callback(err, result.rowCount ? result.rows[0] : false);
+			done();
+		});
+	});
+};
+
+// This will very much depend on your setup, I wouldn't advise doing anything exactly like this but
+// it gives an example of how to use the method to resrict certain grant types
+var authorizedClientIds = ['abc1', 'def2'];
+model.grantTypeAllowed = function (clientId, grantType, callback) {
+	if (grantType === 'password') {
+		return callback(false, authorizedClientIds.indexOf(clientId.toLowerCase()) >= 0);
+	}
+
+	callback(false, true);
+};
+
+model.saveAccessToken = function (accessToken, clientId, userId, expires, callback) {
+	pg.connect(connString, function (err, client, done) {
+		if (err) return callback(err);
+		client.query('INSERT INTO oauth_access_tokens(access_token, client_id, user_id, expires) ' +
+				'VALUES ($1, $2, $3, $4)', [accessToken, clientId, userId, expires],
+				function (err, result) {
+			callback(err);
+			done();
+		});
+	});
+};
+
+model.saveRefreshToken = function (refreshToken, clientId, userId, expires, callback) {
+	pg.connect(connString, function (err, client, done) {
+		if (err) return callback(err);
+		client.query('INSERT INTO oauth_refresh_tokens(refresh_token, client_id, user_id, ' +
+				'expires) VALUES ($1, $2, $3, $4)', [accessToken, clientId, userId, expires],
+				function (err, result) {
+			callback(err);
+			done();
+		});
+	});
+};
+
+/*
+ * Required to support password grant type
+ */
+model.getUser = function (username, password, callback) {
+	pg.connect(connString, function (err, client, done) {
+		if (err) return callback(err);
+		client.query('SELECT id FROM users WHERE username = $1 AND password = $2', [username,
+				password], function (err, result) {
+			callback(err, result.rowCount ? result.rows[0] : false);
+			done();
+		});
+	});
+};
