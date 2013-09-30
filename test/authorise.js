1e7b401e92e347821ce499f3c6aa185e3d98cfd7
Thom Seddon <thom@nightworld.com>
2013-09-30 10:24:47 +0100

    Change invalid_token to 401 (Fixes #5)

--- test/authorise.js
+++ test/authorise.js
@@ -62,7 +62,7 @@ describe('Authorise', function() {
       request(app)
         .get('/')
         .set('Authorization', 'Bearer thom')
-        .expect(400, /the access token provided is invalid/i, done);
+        .expect(401, /the access token provided is invalid/i, done);
     });
 
     it('should detect malformed header', function (done) {
@@ -91,7 +91,7 @@ describe('Authorise', function() {
         .post('/')
         .set('Content-Type', 'application/x-www-form-urlencoded')
         .send({ access_token: 'thom' })
-        .expect(400, /the access token provided is invalid/i, done);
+        .expect(401, /the access token provided is invalid/i, done);
     });
 
     it('should not allow GET when access token in body', function (done) {
@@ -108,7 +108,7 @@ describe('Authorise', function() {
 
       request(app)
         .get('/?access_token=thom')
-        .expect(400, /the access token provided is invalid/i, done);
+        .expect(401, /the access token provided is invalid/i, done);
     });
 
     it('should allow exactly one method (get: query + auth)', function (done) {
@@ -137,7 +137,7 @@ describe('Authorise', function() {
 
       request(app)
         .get('/?access_token=thom')
-        .expect(400, /the access token provided is invalid/i, done);
+        .expect(401, /the access token provided is invalid/i, done);
     });
 
     it('should detect invalid token', function (done){
@@ -151,7 +151,7 @@ describe('Authorise', function() {
 
       request(app)
         .get('/?access_token=thom')
-        .expect(400, /the access token provided has expired/i, done);
+        .expect(401, /the access token provided has expired/i, done);
     });
 
     it('should passthrough with a valid token', function (done){
