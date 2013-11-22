63578dfd758db9afafb874c812e04ae8cffc4ac1
Alexandre JARNOUX <alex.jarnoux@gmail.com>
2013-11-22 15:28:10 +0100

    Added mongo db model and Readme

---
+++ examples/mongodb/Readme.md
@@ -0,0 +1,47 @@
+# MongoDB Example
+
+You will need to initialize a Mongoose connection to a mongo db beforehand.
+
+For example :
+
+```js
+...
+
+var mongoose = require('mongoose');
+
+var uristring = 'mongodb://localhost/test';
+
+// Makes connection asynchronously. Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+	if (err) { 
+		console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+	} else {
+		console.log ('Succeeded connected to: ' + uristring);
+	}
+});
+
+...
+```
+
+The object exposed in model.js could be directly passed into the model paramater of the config
+object when initiating.
+
+For example:
+
+```js
+...
+
+app.configure(function() {
+	var oauth = oauthserver({
+		model: require('./model'),
+		grants: ['password'],
+		debug: true
+	});
+	app.use(express.bodyParser());
+	app.use(oauth.handler());
+	app.use(oauth.errorHandler());
+});
+
+...
+```
---
+++ examples/mongodb/model.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright 2013-present NightWorld.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var mongoose = require('mongoose')
+	Schema = mongoose.Schema,
+	model = module.exports;
+
+//
+// Schemas definitions
+//
+var OAuthAccessTokensSchema = new Schema({
+	access_token: { type: String },
+	client_id: { type: String },
+	user_id: { type: String },
+	expires: { type: Date }
+});
+
+var OAuthClientsSchema = new Schema({
+	client_id: { type: String },
+	client_secret: { type: String },
+	redirect_uri: { type: String }
+});
+
+var OAuthUsersSchema = new Schema({
+	id: { type: Number },
+	username: { type: String },
+	password: { type: String },
+	firstname: { type: String },
+	lastname: { type: String },
+	email: { type: String, default: '' }
+});
+
+mongoose.model('OAuthAccessTokens', OAuthAccessTokensSchema);
+mongoose.model('OAuthClients', OAuthClientsSchema);
+mongoose.model('OAuthUsers', OAuthUsersSchema);
+
+var OAuthAccessTokensModel = mongoose.model('OAuthAccessTokens'),
+	OAuthClientsModel = mongoose.model('OAuthClients'),
+	OAuthUsersModel = mongoose.model('OAuthUsers');
+
+//
+// node-oauth2-server callbacks
+//
+model.getAccessToken = function (bearerToken, callback) {
+	console.log('in getAccessToken (bearerToken: ' + bearerToken + ')');
+
+	OAuthAccessTokensModel.findOne({ access_token: bearerToken }, function (err, accessToken) {
+		if (err) {
+			return callback(err);
+		}
+		callback(null, accessToken);
+	});
+};
+
+model.getClient = function (clientId, clientSecret, callback) {
+	console.log('in getClient (clientId: ' + clientId + ', clientSecret: ' + clientSecret + ')');
+
+	OAuthClientsModel.findOne({ client_id: clientId, client_secret: clientSecret }, function (err, client) {
+		if (err) {
+			return callback(err);
+		}
+		callback(null, client);
+	});
+};
+
+// This will very much depend on your setup, I wouldn't advise doing anything exactly like this but
+// it gives an example of how to use the method to resrict certain grant types
+var authorizedClientIds = ['s6BhdRkqt3', 'toto'];
+model.grantTypeAllowed = function (clientId, grantType, callback) {
+	console.log('in grantTypeAllowed (clientId: ' + clientId + ', grantType: ' + grantType + ')');
+
+	if (grantType === 'password') {
+		return callback(false, authorizedClientIds.indexOf(clientId) >= 0);
+	}
+
+	callback(false, true);
+};
+
+model.saveAccessToken = function (accessToken, clientId, userId, expires, callback) {
+	console.log('in saveAccessToken (accessToken: ' + accessToken + ', clientId: ' + clientId + ', userId: ' + userId + ', expires: ' + expires + ')');
+	
+	var accessToken = new OAuthAccessTokensModel({
+		access_token: accessToken,
+		client_id: clientId,
+		user_id: userId,
+		expires: expires
+	});
+
+	accessToken.save(accessToken, function (err, token) {
+		if (err) {
+			return callback(err);
+		}
+	});
+
+	return callback(null);
+};
+
+/*
+ * Required to support password grant type
+ */
+model.getUser = function (username, password, callback) {
+	console.log('in getUser (username: ' + username + ', password: ' + password + ')');
+
+	OAuthUsersModel.findOne({ username: username, password: password }, function (err, user) {
+		if (err) {
+			return callback(err);
+		}
+		return callback(null, user);
+	});
+};
