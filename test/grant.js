090fdcaaa603a6238405caa8c3c79c36cf3097a9
Luckner Jr Jean-Baptiste <luckner@gmail.com>
2014-07-22 13:00:02 -0400

    removed extra function after upstream merge

--- test/grant.js
+++ test/grant.js
@@ -282,39 +282,6 @@ describe('Grant', function() {
 
     });
 
-    it('should accept the client_credentials grant type', function (done) {
-      var app = bootstrap({
-        model: {
-          getClient: function (id, secret, callback) {
-            callback(false, true);
-          },
-          grantTypeAllowed: function (clientId, grantType, callback) {
-            callback(false, true);
-          },
-          getUserFromClient: function (clientId, clientSecret, callback) {
-            callback(false, { id: 1 });
-          },
-          generateToken: function (type, req, callback) {
-            callback(false, 'thommy');
-          },
-          saveAccessToken: function (token, clientId, expires, user, cb) {
-            token.should.equal('thommy');
-            cb();
-          }
-        },
-        grants: ['client_credentials']
-      });
-
-      request(app)
-        .post('/oauth/token')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send({ grant_type: 'client_credentials' })
-        .set('Authorization', 'Basic dGhvbTpuaWdodHdvcmxk')
-        .expect(/thommy/, 200, done);
-
-    });
-  });
-
   describe('saving access token', function () {
     it('should pass valid params to model.saveAccessToken', function (done) {
       var app = bootstrap({
