b51fb4166ebdb5d2d07b57e6e6aaec2dbd0568b8
cesine <cesine@yahoo.com>
2015-05-12 17:11:47 -0400

    tests said null expiry was possible

--- examples/postgresql/schema.sql
+++ examples/postgresql/schema.sql
@@ -36,7 +36,7 @@ CREATE TABLE oauth_access_tokens (
     access_token text NOT NULL,
     client_id text NOT NULL,
     user_id uuid NOT NULL,
-    expires timestamp without time zone NOT NULL
+    expires timestamp without time zone
 );
 
 
@@ -59,7 +59,7 @@ CREATE TABLE oauth_refresh_tokens (
     refresh_token text NOT NULL,
     client_id text NOT NULL,
     user_id uuid NOT NULL,
-    expires timestamp without time zone NOT NULL
+    expires timestamp without time zone
 );
 
 
--- examples/postgresql/testData.sql
+++ examples/postgresql/testData.sql
@@ -4,3 +4,5 @@
 
 INSERT INTO users(id, username, password, firstname) VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'thom', 'nightW0r1d', 'Thom');
 INSERT INTO oauth_access_tokens(access_token, client_id, user_id, expires) VALUES ('JKNW-9382j-JK83h2-ak3aiUIW', 'afancyagregatorapp', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', TIMESTAMP '2019-01-08 04:05:06');
+INSERT INTO oauth_access_tokens(access_token, client_id, user_id, expires) VALUES ('test-9382j-JK83h2-expired', 'afancyagregatorapp', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', TIMESTAMP '2000-01-08 04:05:06');
+INSERT INTO oauth_access_tokens(access_token, client_id, user_id, expires) VALUES ('test-9382j-never-expired', 'afancyagregatorapp', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', null);
