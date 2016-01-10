db73196087b232f35c0d2c525f4e35668e83457f
Nuno Sousa <nunofgs@gmail.com>
2016-01-10 16:44:50 +0000

    Remove refresh token from client credentials grant
    Fixes #174

--- test/unit/grant-types/client-credentials-grant-type_test.js
+++ test/unit/grant-types/client-credentials-grant-type_test.js
@@ -42,15 +42,13 @@ describe('ClientCredentialsGrantType', function() {
       var handler = new ClientCredentialsGrantType({ accessTokenLifetime: 120, model: model });
 
       sinon.stub(handler, 'generateAccessToken').returns('foo');
-      sinon.stub(handler, 'generateRefreshToken').returns('bar');
       sinon.stub(handler, 'getAccessTokenExpiresAt').returns('biz');
-      sinon.stub(handler, 'getRefreshTokenExpiresAt').returns('baz');
 
       return handler.saveToken(user, client, 'foobar')
         .then(function() {
           model.saveToken.callCount.should.equal(1);
           model.saveToken.firstCall.args.should.have.length(3);
-          model.saveToken.firstCall.args[0].should.eql({ accessToken: 'foo', accessTokenExpiresAt: 'biz', refreshToken: 'bar', refreshTokenExpiresAt: 'baz', scope: 'foobar' });
+          model.saveToken.firstCall.args[0].should.eql({ accessToken: 'foo', accessTokenExpiresAt: 'biz', scope: 'foobar' });
           model.saveToken.firstCall.args[1].should.equal(client);
           model.saveToken.firstCall.args[2].should.equal(user);
         })
