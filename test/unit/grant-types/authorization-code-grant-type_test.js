3a01425f2a648f2f4584abb9b1cfa8d500f27208
Max Truxa <dev@maxtruxa.com>
2016-07-08 19:15:54 +0200

    Add token lifetimes for the auth code grant

--- test/unit/grant-types/authorization-code-grant-type_test.js
+++ test/unit/grant-types/authorization-code-grant-type_test.js
@@ -70,12 +70,14 @@ describe('AuthorizationCodeGrantType', function() {
       sinon.stub(handler, 'validateScope').returns('foobiz');
       sinon.stub(handler, 'generateAccessToken').returns(Promise.resolve('foo'));
       sinon.stub(handler, 'generateRefreshToken').returns(Promise.resolve('bar'));
+      sinon.stub(handler, 'getAccessTokenExpiresAt').returns(Promise.resolve('biz'));
+      sinon.stub(handler, 'getRefreshTokenExpiresAt').returns(Promise.resolve('baz'));
 
-      return handler.saveToken(user, client, 'foobar', 'foobiz')
+      return handler.saveToken(user, client, 'foobiz')
         .then(function() {
           model.saveToken.callCount.should.equal(1);
           model.saveToken.firstCall.args.should.have.length(3);
-          model.saveToken.firstCall.args[0].should.eql({ accessToken: 'foo', authorizationCode: 'foobar', refreshToken: 'bar', scope: 'foobiz' });
+          model.saveToken.firstCall.args[0].should.eql({ accessToken: 'foo', accessTokenExpiresAt: 'biz', refreshToken: 'bar', refreshTokenExpiresAt: 'baz', scope: 'foobiz' });
           model.saveToken.firstCall.args[1].should.equal(client);
           model.saveToken.firstCall.args[2].should.equal(user);
         })
