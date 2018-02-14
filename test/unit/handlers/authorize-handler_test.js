c33a2cd97b44220261c3f51e7b764081cb7d80ab
Marco Lüthy <marco@luethy.net>
2018-02-14 14:51:22 +0100

    resolve merge conflict

--- test/unit/handlers/authorize-handler_test.js
+++ test/unit/handlers/authorize-handler_test.js
@@ -111,26 +111,4 @@ describe('AuthorizeHandler', function() {
         .catch(should.fail);
     });
   });
-
-  describe('saveAuthorizationCode()', function() {
-    it('should call `model.saveAuthorizationCode()`', function() {
-      var model = {
-        getAccessToken: function() {},
-        getClient: function() {},
-        saveAuthorizationCode: sinon.stub().returns({})
-      };
-      var handler = new AuthorizeHandler({ authorizationCodeLifetime: 120, model: model });
-
-      return handler.saveAuthorizationCode('foo', 'bar', 'qux', 'biz', 'baz', 'boz')
-        .then(function() {
-          model.saveAuthorizationCode.callCount.should.equal(1);
-          model.saveAuthorizationCode.firstCall.args.should.have.length(3);
-          model.saveAuthorizationCode.firstCall.args[0].should.eql({ authorizationCode: 'foo', expiresAt: 'bar', redirectUri: 'baz', scope: 'qux' });
-          model.saveAuthorizationCode.firstCall.args[1].should.equal('biz');
-          model.saveAuthorizationCode.firstCall.args[2].should.equal('boz');
-          model.saveAuthorizationCode.firstCall.thisValue.should.equal(model);
-        })
-        .catch(should.fail);
-    });
-  });
 });
